fe43a49c75c4928125571702f49cc6ea64be20a0
unisanta <unisanta@M228-10>
2025-02-13 21:38:10 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoPlanta_Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
